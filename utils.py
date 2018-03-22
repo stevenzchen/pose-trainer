0a2c99d84460f7b15ee435ea2d1f2674ae0fc018
Richard Yang <richard.yang@cs.stanford.edu>
2018-03-22 00:35:30 -0700

    DTW and KNN model for analysis

--- utils.py
+++ utils.py
@@ -21,3 +21,26 @@ def files_in_order(folderpath):
 
     npy_files = np.array(npy_files)
     return npy_files[indices]
+
+# Generates binary labels (good=1, bad=0) given an array-like of filenames
+def get_labels(array):
+    labels = [1 if "good" in i else 0 for i in array]
+    return np.array(labels)
+
+# Compute Dynamic Time Warp Distance of two sequences
+# http://alexminnaar.com/time-series-classification-and-clustering-with-python.html
+def DTWDistance(s1, s2):
+    DTW={}
+
+    for i in range(len(s1)):
+        DTW[(i, -1)] = float('inf')
+    for i in range(len(s2)):
+        DTW[(-1, i)] = float('inf')
+    DTW[(-1, -1)] = 0
+
+    for i in range(len(s1)):
+        for j in range(len(s2)):
+            dist= (s1[i]-s2[j])**2
+            DTW[(i, j)] = dist + min(DTW[(i-1, j)],DTW[(i, j-1)], DTW[(i-1, j-1)])
+
+    return np.sqrt(DTW[len(s1)-1, len(s2)-1])
